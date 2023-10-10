f4f29f1861a5aa1522a5bf206881ff87ead9a7f0
yeongjuJeon <kgkt205@gmail.com>
2023-10-10 15:39:38 +0900

    chapter1 변수 인라인하기

--- chapter01/1-1/statement.mjs
+++ chapter01/1-1/statement.mjs
@@ -36,8 +36,6 @@ export default function statement(invoice, plays) {
   }
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf); // 추출한 함수 이용
-
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
@@ -46,10 +44,10 @@ export default function statement(invoice, plays) {
     }
 
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(thisAmount / 100)} ${
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} ${
       perf.audience
     }석\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
   result += `총액 ${format(totalAmount / 100)}\n`;
   result += `적립 포인트 ${volumeCredits}점\n`;
