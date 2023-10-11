2600dcf4e0dec11208f159d02b38657d3180477a
yeongjuJeon <kgkt205@gmail.com>
2023-10-11 16:28:50 +0900

    chapter1 for 반복문 파이프라인으로 바꾸기

--- chapter01/1-1/statement.mjs
+++ chapter01/1-1/statement.mjs
@@ -7,19 +7,11 @@ export default function statement(invoice, plays) {
   return renderPlainText(statementData, plays);
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0); // for 반복문을 파이프라인으로 바꿈
   }
 
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0); // for 반복문을 파이프라인으로 바꿈
   }
 
   function enrichPerformance(aPerformance) {
